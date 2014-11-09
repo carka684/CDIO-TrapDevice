5add20b47c05f4d1a5bf0da792f244ad026a7bfb
Tobias Norlund <tobias.norlund@tele2.se>
2014-11-09 23:16:47 +0100

    Visar bilder på identifierade objekt i appen

--- src/edu/wildlifesecurity/trapdevice/MainActivity.java
+++ src/edu/wildlifesecurity/trapdevice/MainActivity.java
@@ -18,6 +18,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import edu.wildlifesecurity.framework.IEventHandler;
 import edu.wildlifesecurity.framework.detection.DetectionEvent;
+import edu.wildlifesecurity.framework.identification.IdentificationEvent;
 
 public class MainActivity extends Activity {
 	
@@ -91,6 +92,47 @@ public class MainActivity extends Activity {
 			    	
 			    });
 			}
+	        if(service.identification != null){
+		        service.identification.addEventHandler(IdentificationEvent.NEW_IDENTIFICATION, new IEventHandler<IdentificationEvent>(){
+	
+		        	private int count = 0;
+		        	
+					@Override
+					public void handle(final IdentificationEvent event) {
+						
+						runOnUiThread(new Runnable() {
+	
+							@Override
+							public void run() {
+								
+								Bitmap bm = Bitmap.createBitmap(event.getResult().getImage().cols(), event.getResult().getImage().rows(),	Bitmap.Config.ARGB_8888);
+								Utils.matToBitmap(event.getResult().getImage(), bm);
+								ImageView iv = null;
+								switch(count % 4){
+								case 0:
+									iv = (ImageView) findViewById(R.id.IdentifiedImage1);
+									break;
+								case 1:
+									iv = (ImageView) findViewById(R.id.IdentifiedImage2);
+									break;
+								case 2:
+									iv = (ImageView) findViewById(R.id.IdentifiedImage3);
+									break;
+								case 3:
+									iv = (ImageView) findViewById(R.id.IdentifiedImage4);
+									break;
+								}
+								count++;
+								
+								iv.setImageBitmap(bm);
+							}
+	
+						});
+	
+					}
+			    	
+			    });
+			}
 	    }
 
 		@Override
@@ -115,7 +157,7 @@ public class MainActivity extends Activity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.IdentifiedImage1) {
 			//Settings
             Intent intent = new Intent(this, SettingsActivity.class);
             startActivity(intent);
